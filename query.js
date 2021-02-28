8d31130fe3503bedcddc779f80361fc60a0e502e
Fuhad Olasunkanmi Raheem <54884175+irewumi@users.noreply.github.com>
2021-02-28 22:53:18 +0100

    Create query.js

---
+++ query.js
@@ -0,0 +1,65 @@
+$(function(){
+    $('#delivery').click(function(event) {
+        $('body, html').animate({
+          scrollTop: $(".main").offset().top
+        }, 600);
+    
+    });
+
+    $('#works').click(function(event) {
+        $('body, html').animate({
+          scrollTop: $(".how-it-works").offset().top
+    }, 600);
+
+    });
+
+    $('#cities').click(function(event) {
+            $('body, html').animate({
+              scrollTop: $(".our-cities").offset().top
+    }, 600);
+    
+    });
+
+    $('#sign').click(function(event) {
+        $('body, html').animate({
+          scrollTop: $(".eating-healthy").offset().top
+    }, 600);
+
+    });
+
+    $('#hungry').click(function(event) {
+        $('body, html').animate({
+          scrollTop: $(".gallery").offset().top
+    }, 600);
+
+    });
+    
+    $('#more').click(function(event) {
+        $('body, html').animate({
+          scrollTop: $(".happy").offset().top
+    }, 600);
+
+    });
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
+  
+});
